0347478ceb10311f3a0ec8fab23d4a8bf493a8b9
Kinoko <huiyuanai129@outlook.com>
2025-12-26 05:21:38 +0800

    1

--- lib/widgets/keyboard_text_field.dart
+++ lib/widgets/keyboard_text_field.dart
@@ -27,13 +27,6 @@ class KeyboardTextField extends StatefulWidget {
 
 class _KeyboardTextFieldState extends State<KeyboardTextField> {
   final FocusNode _focusNode = FocusNode();
-  bool _isHandlingDelete = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode.onKey = _onKey;
-  }
 
   @override
   void dispose() {
@@ -42,29 +35,25 @@ class _KeyboardTextFieldState extends State<KeyboardTextField> {
   }
 
   void _handleDelete() {
-    if (_isHandlingDelete) return; // 防止重复处理
-
     final c = widget.controller;
     final sel = c.selection;
     final text = c.text;
-
     if (sel.isValid) {
       if (sel.start != sel.end) {
-        // 有选中文本，删除选中的部分
         final newText = text.replaceRange(sel.start, sel.end, '');
         c.text = newText;
         c.selection = TextSelection.collapsed(offset: sel.start);
+        return;
       } else {
-        // 没有选中文本，删除光标前的一个字符
         if (sel.start > 0) {
           final start = sel.start - 1;
           final newText = text.replaceRange(start, sel.start, '');
           c.text = newText;
           c.selection = TextSelection.collapsed(offset: start);
+          return;
         }
       }
     } else {
-      // 选择无效，删除最后一个字符
       if (text.isNotEmpty) {
         final newText = text.substring(0, text.length - 1);
         c.text = newText;
@@ -73,29 +62,30 @@ class _KeyboardTextFieldState extends State<KeyboardTextField> {
     }
   }
 
-  KeyEventResult _onKey(FocusNode node, RawKeyEvent event) {
+  bool _onKey(RawKeyEvent event) {
     if (event is RawKeyDownEvent) {
       final key = event.logicalKey;
       if (key == LogicalKeyboardKey.delete || key == LogicalKeyboardKey.backspace) {
-        _isHandlingDelete = true;
         _handleDelete();
-        _isHandlingDelete = false;
-        return KeyEventResult.handled;
+        return true;
       }
     }
-    return KeyEventResult.ignored;
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: widget.controller,
+    return RawKeyboardListener(
       focusNode: _focusNode,
-      validator: widget.validator,
-      decoration: widget.decoration,
-      obscureText: widget.obscureText,
-      keyboardType: widget.keyboardType,
-      maxLines: widget.maxLines,
+      onKey: _onKey,
+      child: TextFormField(
+        controller: widget.controller,
+        validator: widget.validator,
+        decoration: widget.decoration,
+        obscureText: widget.obscureText,
+        keyboardType: widget.keyboardType,
+        maxLines: widget.maxLines,
+      ),
     );
   }
 }
